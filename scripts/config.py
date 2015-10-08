c576b9f3f3e8482a99d3a7df85f3960762362887
Yoshihiro Hasebe <y-hasebe@ah.jp.nec.com>
2015-10-08 11:34:36 +0900

    add copyright notice

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # Copyright (c) 2011 Stanford University
+# Copyright (c) 2014-2015 NEC Corporation
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
